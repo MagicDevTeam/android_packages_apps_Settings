1771f40856d174ce71f8e4ba0201986ca53a6a78
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:49:47 +0530

    Settings(MSIM): Add support for MSIM
    Add Multi SIM Settings screen in UI which is used to select
    Voice, SMS, Data preferences.
    Extend APN Settings, Security Settings, Wireless Settings
    for MSIM.
    
    Change-Id: I84abe80f20de2415ff819c7250ef0a0799662b52
    
    Conflicts:
    	res/values/arrays.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/DeviceInfoSettings.java
    	src/com/android/settings/SecuritySettings.java
    	src/com/android/settings/Settings.java
    
    Settings(MSIM): Add support for MSIM ----- fix typo

--- src/com/android/settings/ApnEditor.java
+++ src/com/android/settings/ApnEditor.java
@@ -34,11 +34,14 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.provider.Telephony;
+import android.telephony.MSimTelephonyManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
@@ -91,6 +94,7 @@ public class ApnEditor extends SettingsPreferenceFragment
 
     private String mCurMnc;
     private String mCurMcc;
+    private int mSubscription = 0;
 
     private Uri mUri;
     private Cursor mCursor;
@@ -202,6 +206,10 @@ public class ApnEditor extends SettingsPreferenceFragment
             mUri = intent.getData();
         }
 
+        // Read the subscription received from Phone settings.
+        mSubscription = intent.getIntExtra(SelectSubscription.SUBSCRIPTION_KEY,
+                MSimTelephonyManager.getDefault().getDefaultSubscription());
+        Log.d(TAG,"ApnEditor onCreate received sub: " + mSubscription);
         mFirstTime = icicle == null;
 
         if (action.equals(Intent.ACTION_INSERT)) {
@@ -503,6 +511,7 @@ public class ApnEditor extends SettingsPreferenceFragment
         String apn = checkNotSet(mApn.getText());
         String mcc = checkNotSet(mMcc.getText());
         String mnc = checkNotSet(mMnc.getText());
+        int dataSub = 0;
 
         if (getErrorMsg() != null && !force) {
             showDialog(ERROR_DIALOG_ID);
@@ -552,8 +561,19 @@ public class ApnEditor extends SettingsPreferenceFragment
 
         values.put(Telephony.Carriers.NUMERIC, mcc + mnc);
 
+        try {
+            dataSub = Settings.Global.getInt(getContentResolver(),
+                    Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION);
+        } catch (SettingNotFoundException snfe) {
+            // Exception Reading Multi Sim Data Subscription Value
+            if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+               Log.e(TAG, "Exception Reading Multi Sim Data Subscription Value.", snfe);
+            }
+        }
+
         if (mCurMnc != null && mCurMcc != null) {
-            if (mCurMnc.equals(mnc) && mCurMcc.equals(mcc)) {
+            if (mCurMnc.equals(mnc) && mCurMcc.equals(mcc) &&
+                    mSubscription == dataSub ) {
                 values.put(Telephony.Carriers.CURRENT, 1);
             }
         }
