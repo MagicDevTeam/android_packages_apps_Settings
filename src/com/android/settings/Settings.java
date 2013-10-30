bfc294957a451d7f23cf6e0e9def28bf0d27b24f
SunRain <5908183@163.com>
2013-10-30 23:09:21 +0800

    Add magic state

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -867,6 +867,7 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class ProfilesSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
