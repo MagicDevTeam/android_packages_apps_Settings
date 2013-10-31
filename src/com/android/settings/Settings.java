eca6e7c2058c91ece3bc6d6178c27f1cfc2dbd03
SunRain <5908183@163.com>
2013-10-31 22:56:00 +0800

    [Settings] Add performance control

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -475,6 +475,10 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
+            } else if (id == R.id.performance_controls) {
+                if (!showDev) {
+                    target.remove(i);
+                }
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
@@ -878,4 +882,5 @@ public class Settings extends PreferenceActivity
     public static class UserSettingsActivity extends Settings { /* empty */ }
     public static class NotificationAccessSettingsActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
