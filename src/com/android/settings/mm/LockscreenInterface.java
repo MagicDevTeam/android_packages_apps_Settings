d851984cbcf7bec275fe9d7dfcbb0e885a3a59f1
SunRain <41245110@qq.com>
2014-01-20 22:25:34 +0800

    [Settings] Add a standalone LockScreen setting in main view
    Change-Id: I77f2a867c9007896b0d72957bcd11a65881a4592

---
+++ src/com/android/settings/mm/LockscreenInterface.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2014 The MagicMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mm;
+
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.ChooseLockSettingsHelper;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.R;
+
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+
+    private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
+
+    private CheckBoxPreference mEnableKeyguardWidgets;
+
+    private ChooseLockSettingsHelper mChooseLockSettingsHelper;
+    private LockPatternUtils mLockUtils;
+    private DevicePolicyManager mDPM;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.mm_lockscreen_interface_settings);
+ 
+        mChooseLockSettingsHelper = new ChooseLockSettingsHelper(getActivity());
+        mLockUtils = mChooseLockSettingsHelper.utils();
+        mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        // Find preferences
+        mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
+
+        // Remove/disable custom widgets based on device RAM and policy
+        if (ActivityManager.isLowRamDeviceStatic()) {
+            // Widgets take a lot of RAM, so disable them on low-memory devices
+            this.getPreferenceScreen().removePreference(findPreference(KEY_ENABLE_WIDGETS));
+            mEnableKeyguardWidgets = null;
+        } else {
+            checkDisabledByPolicy(mEnableKeyguardWidgets,
+                    DevicePolicyManager.KEYGUARD_DISABLE_WIDGETS_ALL);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Update custom widgets and camera
+        if (mEnableKeyguardWidgets != null) {
+            mEnableKeyguardWidgets.setChecked(mLockUtils.getWidgetsEnabled());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        final String key = preference.getKey();
+
+        if (KEY_ENABLE_WIDGETS.equals(key)) {
+            mLockUtils.setWidgetsEnabled(mEnableKeyguardWidgets.isChecked());
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        //ContentResolver cr = getActivity().getContentResolver();
+
+        return false;
+    }
+
+      /**
+     * Checks if a specific policy is disabled by a device administrator, and disables the
+     * provided preference if so.
+     * @param preference Preference
+     * @param feature Feature
+     */
+    private void checkDisabledByPolicy(Preference preference, int feature) {
+        boolean disabled = featureIsDisabled(feature);
+
+        if (disabled) {
+            preference.setSummary(R.string.security_enable_widgets_disabled_summary);
+        }
+
+        preference.setEnabled(!disabled);
+    }
+
+    /**
+     * Checks if a specific policy is disabled by a device administrator.
+     * @param feature Feature
+     * @return Is disabled
+     */
+    private boolean featureIsDisabled(int feature) {
+        return (mDPM.getKeyguardDisabledFeatures(null) & feature) != 0;
+    }
+
+}
